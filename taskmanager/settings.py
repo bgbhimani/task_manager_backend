3e5f7bddab1e5469cf4fcbe0423c5a453463e8dd
Bhavik Bhimani <166936122+bgbhimani@users.noreply.github.com>
2025-08-11 22:37:13 +0530

    Admin Tables And Responsive

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fz^dp+=9xpz+9s=1f@2p*z0sw1=2p^2_tpakh844^vlechur74')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fz^fdp+=9rxpzgrj+9s=1f@f2yep*z0swgdfg1=2p^2_tpakh844^vlechur74')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
@@ -136,15 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build', 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'build', 'static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Make sure Django finds index.html for frontend routes
-TEMPLATES[0]['DIRS'] = [
-    os.path.join(BASE_DIR, 'build'),
-]
+# TEMPLATES[0]['DIRS'] = [
+#     os.path.join(BASE_DIR, 'build'),
+# ]
 
 
 
